2fc1ee8bd6622a2cb980ac110cd7af5d26f4eeaa
maxOn <maksplatonov07@mail.ru>
2026-05-05 21:47:18 +0400

    сделал старницу занятий

--- src/pages/classes.tsx
+++ src/pages/classes.tsx
@@ -1,8 +1,314 @@
+import { Link } from "react-router-dom"
 
-function Classes(){
-    return(
+function Classes() {
+    return (
         <>
-            
+            <div className="block1c mx-0">
+                <div className="block1cCT">
+                    <div className="block1cBT">
+                        Наши занятия
+                    </div>
+                    <div className="block1cT">
+                        Откройте для себя множество занятий, которые помогут вам стать сильнее, выносливее и увереннее
+                    </div>
+                </div>
+            </div>
+            <div className="block2c">
+                <div className="row tablesC mx-0 gap-5 pb-5">
+                    <div className="col px-0 tableC">
+                        <div className="tableImageC1"></div>
+                        <div className="tableTextCW">
+                            <div className="tableTextC">
+                                <div className="tableNameC pt-2">Йога</div>
+                                <div className="tableDescC pb-1">Динамичная последовательность поз для развития силы, гибкости и осознанности.</div>
+                                <div className="d-flex tableMiniC">
+                                    <div className="imageC1"></div>
+                                    <div className="tableMiniDescC d-flex">45 Мин</div>
+                                </div>
+                                <div className="d-flex tableMiniC">
+                                    <div className="imageC2"></div>
+                                    <div className="tableMiniDescC">Макс. 20 человек</div>
+                                </div>
+                                <div className="d-flex tableMiniC3">
+                                    <div className="imageC3"></div>
+                                    <div className="tableMiniDescC">Начальный уровень</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="scheduleC">
+                            Расписание
+                        </div>
+                        <div className="scheduleBlocksC d-flex">
+                            <div className="scheduleBlockC1">Понедельник 19:00</div>
+                            <div className="scheduleBlockC">Среда 19:00</div>
+                            <div className="scheduleBlockC">Пятница 19:00</div>
+                        </div>
+                        <div className="moreButtonW">
+                            <Link to="/"><button className="moreButton">Подробнее</button></Link>
+                        </div>
+                    </div>
+                    <div className="col px-0 tableC">
+                        <div className="tableImageC2"></div>
+                        <div className="tableTextCW">
+                            <div className="tableTextC">
+                                <div className="tableNameC pt-2">Силовая тренировка</div>
+                                <div className="tableDescC pb-1">Нарастите мышечную массу и увеличьте силу с помощью нашей программы силовых тренировок.</div>
+                                <div className="d-flex tableMiniC">
+                                    <div className="imageC1"></div>
+                                    <div className="tableMiniDescC d-flex">90 Мин</div>
+                                </div>
+                                <div className="d-flex tableMiniC">
+                                    <div className="imageC2"></div>
+                                    <div className="tableMiniDescC">Макс. 10 человек</div>
+                                </div>
+                                <div className="d-flex tableMiniC3">
+                                    <div className="imageC3"></div>
+                                    <div className="tableMiniDescC">Средний уровень</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="scheduleC">
+                            Расписание
+                        </div>
+                        <div className="scheduleBlocksC d-flex">
+                            <div className="scheduleBlockC1">Понедельник 7:00</div>
+                            <div className="scheduleBlockC">Среда 7:00</div>
+                            <div className="scheduleBlockC">Пятница 7:00</div>
+                        </div>
+                        <div className="moreButtonW">
+                            <Link to="/"><button className="moreButton">Подробнее</button></Link>
+                        </div>
+                    </div>
+                    <div className="col px-0 tableC">
+                        <div className="tableImageC3"></div>
+                        <div className="tableTextCW">
+                            <div className="tableTextC">
+                                <div className="tableNameC pt-2">Кардио</div>
+                                <div className="tableDescC pb-1">Повысьте частоту сердечных сокращений и выносливость.</div>
+                                <div className="d-flex tableMiniC">
+                                    <div className="imageC1"></div>
+                                    <div className="tableMiniDescC d-flex">45 Мин</div>
+                                </div>
+                                <div className="d-flex tableMiniC">
+                                    <div className="imageC2"></div>
+                                    <div className="tableMiniDescC">Макс. 20 человек</div>
+                                </div>
+                                <div className="d-flex tableMiniC3">
+                                    <div className="imageC3"></div>
+                                    <div className="tableMiniDescC">Начальный уровень</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="scheduleC">
+                            Расписание
+                        </div>
+                        <div className="scheduleBlocksC d-flex">
+                            <div className="scheduleBlockC1">Понедельник 18:30</div>
+                            <div className="scheduleBlockC">Среда 18:30</div>
+                            <div className="scheduleBlockC">Пятница 18:30</div>
+                        </div>
+                        <div className="moreButtonW">
+                            <Link to="/"><button className="moreButton">Подробнее</button></Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="row tablesC mx-0 gap-5 pb-5">
+                    <div className="col px-0 tableC">
+                        <div className="tableImageC4"></div>
+                        <div className="tableTextCW">
+                            <div className="tableTextC">
+                                <div className="tableNameC pt-2">Бокс</div>
+                                <div className="tableDescC pb-1">Классическая ударная тренировка, развивающая координацию, скорость, выносливость и силу удара</div>
+                                <div className="d-flex tableMiniC">
+                                    <div className="imageC1"></div>
+                                    <div className="tableMiniDescC d-flex">55 мин</div>
+                                </div>
+                                <div className="d-flex tableMiniC">
+                                    <div className="imageC2"></div>
+                                    <div className="tableMiniDescC">Макс. 14 человек</div>
+                                </div>
+                                <div className="d-flex tableMiniC3">
+                                    <div className="imageC3"></div>
+                                    <div className="tableMiniDescC">Начальный уровень</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="scheduleC">
+                            Расписание
+                        </div>
+                        <div className="scheduleBlocksC d-flex">
+                            <div className="scheduleBlockC1">Понедельник 17:00</div>
+                            <div className="scheduleBlockC">Среда 17:00</div>
+                            <div className="scheduleBlockC">Пятница 17:00</div>
+                        </div>
+                        <div className="moreButtonW">
+                            <Link to="/"><button className="moreButton">Подробнее</button></Link>
+                        </div>
+                    </div>
+                    <div className="col px-0 tableC">
+                        <div className="tableImageC5"></div>
+                        <div className="tableTextCW">
+                            <div className="tableTextC">
+                                <div className="tableNameC pt-2">HIIT цикл</div>
+                                <div className="tableDescC pb-1">Круговая тренировка высокой интенсивности для максимального жиросжигания.</div>
+                                <div className="d-flex tableMiniC">
+                                    <div className="imageC1"></div>
+                                    <div className="tableMiniDescC d-flex">40 мин</div>
+                                </div>
+                                <div className="d-flex tableMiniC">
+                                    <div className="imageC2"></div>
+                                    <div className="tableMiniDescC">Макс. 16 человек</div>
+                                </div>
+                                <div className="d-flex tableMiniC3">
+                                    <div className="imageC3"></div>
+                                    <div className="tableMiniDescC">Продвинутый уровень</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="scheduleC">
+                            Расписание
+                        </div>
+                        <div className="scheduleBlocksC d-flex">
+                            <div className="scheduleBlockC1">Вторник 18:00</div>
+                            <div className="scheduleBlockC">Четверг 18:00</div>
+                            <div className="scheduleBlockC">Суббота 18:00</div>
+                        </div>
+                        <div className="moreButtonW">
+                            <Link to="/"><button className="moreButton">Подробнее</button></Link>
+                        </div>
+                    </div>
+                    <div className="col px-0 tableC">
+                        <div className="tableImageC6"></div>
+                        <div className="tableTextCW">
+                            <div className="tableTextC">
+                                <div className="tableNameC pt-2">Велотренажер</div>
+                                <div className="tableDescC pb-1">Энергичная велотренировка с переменной нагрузкой для сильных ног и мощного кардио.</div>
+                                <div className="d-flex tableMiniC">
+                                    <div className="imageC1"></div>
+                                    <div className="tableMiniDescC d-flex">50 мин</div>
+                                </div>
+                                <div className="d-flex tableMiniC">
+                                    <div className="imageC2"></div>
+                                    <div className="tableMiniDescC">Макс. 15 человек</div>
+                                </div>
+                                <div className="d-flex tableMiniC3">
+                                    <div className="imageC3"></div>
+                                    <div className="tableMiniDescC">Средний уровень</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="scheduleC">
+                            Расписание
+                        </div>
+                        <div className="scheduleBlocksC d-flex">
+                            <div className="scheduleBlockC1">Понедельник 15:00</div>
+                            <div className="scheduleBlockC">Среда 15:00</div>
+                            <div className="scheduleBlockC">Пятница 15:00</div>
+                        </div>
+                        <div className="moreButtonW">
+                            <Link to="/"><button className="moreButton">Подробнее</button></Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="row tablesC mx-0 gap-5">
+                    <div className="col px-0 tableC">
+                        <div className="tableImageC7"></div>
+                        <div className="tableTextCW">
+                            <div className="tableTextC">
+                                <div className="tableNameC pt-2">Акробатическая йога</div>
+                                <div className="tableDescC pb-1">Парная практика, сочетающая элементы йоги, акробатики и тайского массажа для доверия и баланса.</div>
+                                <div className="d-flex tableMiniC">
+                                    <div className="imageC1"></div>
+                                    <div className="tableMiniDescC d-flex">75 мин</div>
+                                </div>
+                                <div className="d-flex tableMiniC">
+                                    <div className="imageC2"></div>
+                                    <div className="tableMiniDescC">Макс. 10 человек (пары)</div>
+                                </div>
+                                <div className="d-flex tableMiniC3">
+                                    <div className="imageC3"></div>
+                                    <div className="tableMiniDescC">Средний уровень</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="scheduleC">
+                            Расписание
+                        </div>
+                        <div className="scheduleBlocksC d-flex">
+                            <div className="scheduleBlockC1">Четверг 15:00</div>
+                            <div className="scheduleBlockC">Пятница 16:00</div>
+                            <div className="scheduleBlockC">Суббота 18:00</div>
+                        </div>
+                        <div className="moreButtonW">
+                            <Link to="/"><button className="moreButton">Подробнее</button></Link>
+                        </div>
+                    </div>
+                    <div className="col px-0 tableC">
+                        <div className="tableImageC8"></div>
+                        <div className="tableTextCW">
+                            <div className="tableTextC">
+                                <div className="tableNameC pt-2">Тренировка всего тела</div>
+                                <div className="tableDescC pb-1">Интенсивная силовая тренировка на все тело с использованием гантелей, гирь и собственного веса для максимальной отдачи.</div>
+                                <div className="d-flex tableMiniC">
+                                    <div className="imageC1"></div>
+                                    <div className="tableMiniDescC d-flex">55 Мин</div>
+                                </div>
+                                <div className="d-flex tableMiniC">
+                                    <div className="imageC2"></div>
+                                    <div className="tableMiniDescC">Макс. 14 человек</div>
+                                </div>
+                                <div className="d-flex tableMiniC3">
+                                    <div className="imageC3"></div>
+                                    <div className="tableMiniDescC">Высокий уровень</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="scheduleC">
+                            Расписание
+                        </div>
+                        <div className="scheduleBlocksC d-flex">
+                            <div className="scheduleBlockC1">Понедельник 19:30</div>
+                            <div className="scheduleBlockC">Среда 19:30</div>
+                            <div className="scheduleBlockC">Пятница 19:30</div>
+                        </div>
+                        <div className="moreButtonW">
+                            <Link to="/"><button className="moreButton">Подробнее</button></Link>
+                        </div>
+                    </div>
+                    <div className="col px-0 tableC">
+                        <div className="tableImageC9"></div>
+                        <div className="tableTextCW">
+                            <div className="tableTextC">
+                                <div className="tableNameC pt-2">Тренировка с собственным весом</div>
+                                <div className="tableDescC pb-1">Силовая тренировка только с собственным весом: отжимания, подтягивания, приседания и статические удержания.</div>
+                                <div className="d-flex tableMiniC">
+                                    <div className="imageC1"></div>
+                                    <div className="tableMiniDescC d-flex">45 Мин</div>
+                                </div>
+                                <div className="d-flex tableMiniC">
+                                    <div className="imageC2"></div>
+                                    <div className="tableMiniDescC">Макс. 20 человек</div>
+                                </div>
+                                <div className="d-flex tableMiniC3">
+                                    <div className="imageC3"></div>
+                                    <div className="tableMiniDescC">Начальный уровень</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="scheduleC">
+                            Расписание
+                        </div>
+                        <div className="scheduleBlocksC d-flex">
+                            <div className="scheduleBlockC1">Вторник 18:00</div>
+                            <div className="scheduleBlockC">Четверг 18:00</div>
+                            <div className="scheduleBlockC">Суббота 18:00</div>
+                        </div>
+                        <div className="moreButtonW">
+                            <Link to="/"><button className="moreButton">Подробнее</button></Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
